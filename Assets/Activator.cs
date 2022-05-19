da9010bfc5f920a3fb9afaef484a480d920b56ab
AgusFeld <101400409+AgusFeld@users.noreply.github.com>
2022-05-19 15:54:33 -0300

    Reapso Parte 1

---
+++ Assets/Activator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+
+    public GameObject myCube;
+
+    public bool wasPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wasPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myCube.SetActive(!myCube.activeInHierarchy);
+            
+            /*if (myCube.activeInHierarchy)
+            {
+                myCube.SetActive(false);
+            }
+            else
+            {
+                myCube.SetActive(true);
+            }*/
+
+            /*if(wasPressed == false)
+            {
+                myCube.SetActive(false);
+                wasPressed = true;
+            }
+            else
+            {
+                myCube.SetActive(true);
+                wasPressed = false;
+            }*/
+        }
+    }
+}
